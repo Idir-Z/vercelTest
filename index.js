d27f2079d3f74a46f215eb9f750fe3be880bcd4c
Idir-ZEGGANE <idir.zeggane@se.univ-bejaia.dz>
2024-09-30 16:44:39 +0100

    final

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 app.get('/home', (req, res) => {
   res.send('heyyy you are home');
 });
-const port  = 3000 || 9001
+const port  = process.env.PORT  || 9001
 app.listen(port, () => {
   console.log(`listening onport ${port}`);
   
