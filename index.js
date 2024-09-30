e082561e5d101cb4b4b3b3d7bc293195c47973cd
Idir-ZEGGANE <idir.zeggane@se.univ-bejaia.dz>
2024-09-30 16:33:59 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// api/index.js
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.get('/home', (req, res) => {
+  res.send('heyyy you are home');
+});
+const port  = 3000 || 9001
+app.listen(port, () => {
+  console.log(`listening onport ${port}`);
+  
+})
+// Export the app as a Vercel serverless function
+
